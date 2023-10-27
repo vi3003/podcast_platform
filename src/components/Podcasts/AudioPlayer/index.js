11a0febdc47991f73142ae6d69a60aee17ac6f99
“vi3003” <vidhijain3003@gmail.com>
2023-10-27 20:46:40 +0530

    final

--- src/components/Podcasts/AudioPlayer/index.js
+++ src/components/Podcasts/AudioPlayer/index.js
@@ -4,15 +4,37 @@ import { FaPlay, FaPause, FaVolumeUp, FaVolumeMute} from 'react-icons/fa'
 
 const AudioPlayer = ({ audioSrc, image }) => {
   const [isPlaying, setIsPlaying] = useState(true);
-  const [isMute, setIsMute] = useState(true);
-  const [duration, setDuration] = useState('');
-  const [currentTime, setCurrentTime] = useState(0);
+  const [isMute, setIsMute] = useState(false);
   const [volume, setVolume] = useState(1);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const audioRef = useRef();
-
+                             
+  useEffect(() => {
+    if (isPlaying) {
+        audioRef.current.play();
+    } else {
+        audioRef.current.pause();
+    }
+  }, [isPlaying]);
+  
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.addEventListener("timeUpdate", handleTimeUpdate);
+    audio.addEventListener("loadedmetadata", handleLoadedmetadata);
+    audio.addEventListener("ended", handleEnded);
+  
+    return () => {
+        audio.removeEventListener("timeUpdate", handleTimeUpdate);
+        audio.removeEventListener("loadedmetadata", handleLoadedmetadata);
+        audio.removeEventListener("ended", handleEnded);
+    }
+  }, []);
+  
   const handleDuration = (e) => {
-    setDuration(e.target.value);
-    audioRef.current.currentTime = e.target.value
+    const newTime = e.target.value;
+    setCurrentTime(newTime);
+    audioRef.current.currentTime = newTime;
   }
 
   const togglePlay = () => {
@@ -26,7 +48,7 @@ const AudioPlayer = ({ audioSrc, image }) => {
 
   const toggleMute = () => {
     if(isMute) {
-        setIsMute(false)
+        setIsMute(false);
     }
     else {
         setIsMute(true)
@@ -35,8 +57,14 @@ const AudioPlayer = ({ audioSrc, image }) => {
 
   const handleVolume = (e) => {
     setVolume(e.target.value);
-    audioRef.current.volume = volume;
+    audioRef.current.volume = e.target.value;
 
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
   };
 
   const handleTimeUpdate = () => {
@@ -53,29 +81,8 @@ const AudioPlayer = ({ audioSrc, image }) => {
   };
 
   useEffect(() => {
-    const audio = audioRef.current;
-    audio.addEventListener("timeUpdate", handleTimeUpdate);
-    audio.addEventListener("loadedmetadata", handleLoadedmetadata);
-    audio.addEventListener("ended", handleEnded);
-  
-    return () => {
-        audio.removeEventListener("timeUpdate", handleTimeUpdate);
-        audio.removeEventListener("loadedmetadata", handleLoadedmetadata);
-        audio.removeEventListener("ended", handleEnded);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isPlaying) {
-        audioRef.current.play();
-    } else {
-        audioRef.current.pause();
-    }
-  }, [isPlaying]);
-
-  useEffect(() => {
-    if (isMute) {
-        audioRef.current.volume = volume;
+    if (!isMute) {
+        audioRef.current.volume = 1;
         setVolume(1);
     } else {
         audioRef.current.volume = 0;
@@ -85,20 +92,26 @@ const AudioPlayer = ({ audioSrc, image }) => {
 
   return (
     <div className='custom-audio-player'>
-        <img src={image} className='display-image-player' />
+        <img src={image} className='display-image-player' alt='img'/>
         <audio ref={audioRef} src={audioSrc} />
-        <p className='icon' onClick={togglePlay}>{isPlaying ?<FaPause/> : <FaPlay />}</p>
+        <p className='icon' onClick={togglePlay}>
+          {isPlaying ? <FaPause/> : <FaPlay />}
+        </p>
         <div className='duration-flex'>
-            <p>0:00</p>
+            <p>{formatTime(currentTime)}</p>
             <input
              type='range' 
+             min="0"
              max={duration}
              value={currentTime}
              onChange={handleDuration}
+             step="0.001"
              className='duration-range'
             />
-            <p>-21:00</p>
-            <p className='icon' onClick={toggleMute} >{isMute ? <FaVolumeUp /> : <FaVolumeMute />}</p>
+            <p>-{formatTime(duration - currentTime)}</p>
+            <p className='icon' onClick={toggleMute} >
+              {!isMute ? <FaVolumeUp /> : <FaVolumeMute />}
+            </p>
             <input
              type='range' 
              value={volume}
